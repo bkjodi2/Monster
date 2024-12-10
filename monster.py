da6308ec510f77773ad82c27c9aed91edaf87e73
bkjodi2 <mohi.t830762@gmail.com>
2024-12-10 20:59:10 +0530

    Update monster.py

--- monster.py
+++ monster.py
@@ -16,7 +16,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '7935132998:AAHo-C0qDfTtOxe-WjyaEZIV_pxzvsyQmhQ'
+TOKEN = '7603132453:AAFXmBGvBk0tTJ69Js6gIjt_X4sFiO-wmhQ'
 MONGO_URI = 'mongodb+srv://Bishal:Bishal@bishal.dffybpx.mongodb.net/?retryWrites=true&w=majority&appName=Bishal'
 FORWARD_CHANNEL_ID = -1002220761952
 CHANNEL_ID = -1002220761952
